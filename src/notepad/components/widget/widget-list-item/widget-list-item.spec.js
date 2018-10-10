2d7a5ae61b45496cfab349c6b0e425386faf9cc7
gusrodriguez <gus.rodriguez@mercadolibre.com>
2018-10-10 19:53:52 -0300

    Added tests for widget-list-item and validations components.

---
+++ src/notepad/components/widget/widget-list-item/widget-list-item.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import WidgetListItem from './';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+  const props = {
+    note: {
+      validations: [],
+    },
+    deleteNote: jest.fn(),
+  };
+
+  const wrapper = mount(<WidgetListItem {...props} />);
+
+  return { props, wrapper };
+}
+
+describe('WidgetListItem', () => {
+  const { wrapper, props } = setup();
+  it('should render correctly', () => {
+    expect(wrapper.find('div').first().hasClass('list__row')).toBe(true);
+  });
+});
