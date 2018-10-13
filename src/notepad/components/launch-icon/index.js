3aebfdb29b6d5d4f82b1e63f8d1a5d6ab123f13c
gusrodriguez <gus.rodriguez@mercadolibre.com>
2018-10-13 11:37:38 -0300

    Fixed wrong background effect on scroll.

--- src/notepad/components/launch-icon/index.js
+++ src/notepad/components/launch-icon/index.js
@@ -41,6 +41,7 @@ class LaunchIcon extends React.Component {
         unmountOnExit
       >
         <aside className='launch-icon' onClick={this.handleClick}>
+          <div className="background" />
           <svg width="80px" height="80px" xmlns='http://www.w3.org/2000/svg' viewBox='17 176.375 48 60'>
             <path d='M41 176.375c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24zm14.814 27.584c0 2.2-1.799 4-4 4H48.77c-2.199 0-5.328 1.215-6.951 2.699l-5.032 4.602c-1.624 1.484-2.952.899-2.952-1.301v-2c0-2.2-1.8-4-4-4h-.149c-2.2 0-4-1.8-4-4v-12.316c0-2.199 1.8-4 4-4h22.13c2.199 0 4 1.801 4 4v12.316h-.002z'
             />
